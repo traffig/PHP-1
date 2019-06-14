45390fc6566ce6264100bf74091daea487a91b1b
Andrey <smoke-999@mail.ru>
2019-06-14 11:21:35 +0400

    update homework3_6
    переделал задание №6 по уроку 3

--- homework3/homework3_6.php
+++ homework3/homework3_6.php
@@ -1,5 +1,5 @@
 <?php
-$array_links = [
+$arr_content = ['array_links' => [
     'Начальная' => '#',
     'Товары' => [
         'Куртки' => '#',
@@ -8,5 +8,22 @@ $array_links = [
     ],
     'О нас' => '#',
     'Вопросы' => '#',
+]
 ];
-include 'template/template_hw6.php';
+
+function renderTemplate($page, array $content = [])
+{
+    ob_start();
+    if (!empty($content)) {
+        extract($content);
+    }
+    $file_name = "views/{$page}.php";
+    if (file_exists($file_name)) {
+        include $file_name;
+    } else {
+        die('Error 404');
+    }
+    return ob_get_clean();
+}
+
+echo renderTemplate('template_hw6', $arr_content);
